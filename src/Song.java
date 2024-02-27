da2c19f5ef02ca93f7fe6259c3b091a9833eab02
christian <christiany2026@student.cis.edu.hk>
2024-02-27 09:51:40 +0800

    woohoo

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners;
+
+    public Song (String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+
+    public int howMany(ArrayList<String> array){
+        for (int i = 0; i < array.size(); i++){
+            array.set(i, array.get(i).toLowerCase());
+        }
+
+        int initial;
+
+        if (listeners == null){
+            listeners = array;
+            initial = 0;
+        }
+
+        else{
+            String test;
+            initial = listeners.size();
+            for (int i = 0; i < array.size(); i++){
+                test = array.get(i);
+                if (!listeners.contains(test)){
+                    listeners.add(test);
+                }
+            }
+        }
+
+        int result = listeners.size();
+
+        return result - initial;
+    }
 }
